a0ee885b997d48f949dbc71405f7124c579a7eaf
prabingupta <prabingupta567@gmail.com>
2026-07-05 07:57:11 +0545

    Register UserProfile and Favorite models in Django admin

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Service, Query
+from .models import Service, Query, UserProfile, Favorite
 
 
 @admin.register(Service)
@@ -14,3 +14,16 @@ class QueryAdmin(admin.ModelAdmin):
     list_display = ["question", "service", "language", "created_at"]
     list_filter = ["language"]
     readonly_fields = ["answer_json", "created_at"]
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "age"]
+    search_fields = ["user__username", "user__first_name"]
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ["user", "service", "created_at"]
+    list_filter = ["service"]
+    search_fields = ["user__username"]
